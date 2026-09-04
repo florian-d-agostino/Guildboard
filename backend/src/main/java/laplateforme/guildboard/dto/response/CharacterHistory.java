d662857aaf52d41bdbfb2b06967e6272311bfef6
florian-d-agostino <florian.d-agostino@laplateforme.io>
2026-09-04 14:49:55 +0200

    feat(dto/character): Finish DTO for character and history

--- backend/src/main/java/laplateforme/guildboard/dto/response/CharacterHistory.java
+++ backend/src/main/java/laplateforme/guildboard/dto/response/CharacterHistory.java
@@ -3,8 +3,8 @@ package laplateforme.guildboard.dto.response;
 import java.time.LocalDateTime;
 
 public record CharacterHistory(
-    long id,
-    String name,
-    LocalDateTime createdAt,
-    LocalDateTime updatedAt
-) {}
+        long id,
+        String questTitle,
+        LocalDateTime assignedAt,
+        LocalDateTime completedAt
+){}
