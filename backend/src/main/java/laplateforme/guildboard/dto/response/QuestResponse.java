78683dba5837d16a8f1f758189ecbc27a3f4ce6d
Hykoo13 <haikmonossiandev@gmail.com>
2026-09-04 15:59:11 +0200

    feat(dto/quest): add missing QuestResponse

--- backend/src/main/java/laplateforme/guildboard/dto/response/QuestResponse.java
+++ backend/src/main/java/laplateforme/guildboard/dto/response/QuestResponse.java
@@ -1,5 +1,16 @@
 package laplateforme.guildboard.dto.response;
 
-public class QuestResponse {
+public record QuestResponse(
 
+        private Long id,
+        private QuestStatus status,
+        private String title,
+        private String description,
+        private QuestDifficulty difficulty,
+        private int minLvl,
+        private int goldReward,
+        private int xpReward,
+        private byte slots,
+        private short successRate,
+        private short completionTime) {
 }
