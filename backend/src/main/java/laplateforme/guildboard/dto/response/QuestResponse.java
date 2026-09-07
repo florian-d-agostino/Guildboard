d86d046eee7defc7e1efe42ee05bdb2cb9903081
Hykoo13 <haikmonossiandev@gmail.com>
2026-09-07 11:14:10 +0200

    feat(dto/quest): complete file QuestResponse

--- backend/src/main/java/laplateforme/guildboard/dto/response/QuestResponse.java
+++ backend/src/main/java/laplateforme/guildboard/dto/response/QuestResponse.java
@@ -1,16 +1,19 @@
 package laplateforme.guildboard.dto.response;
 
+import laplateforme.guildboard.model.enums.QuestStatus;
+import laplateforme.guildboard.model.enums.QuestDifficulty;
+
 public record QuestResponse(
 
-        private Long id,
-        private QuestStatus status,
-        private String title,
-        private String description,
-        private QuestDifficulty difficulty,
-        private int minLvl,
-        private int goldReward,
-        private int xpReward,
-        private byte slots,
-        private short successRate,
-        private short completionTime) {
+        Long id,
+        QuestStatus status,
+        String title,
+        String description,
+        QuestDifficulty difficulty,
+        int minLvl,
+        int goldReward,
+        int xpReward,
+        byte slots,
+        short successRate,
+        short completionTime) {
 }
