ba7f34ec00ba1c0454516e50251fca5a36edc201
Hykoo13 <haikmonossiandev@gmail.com>
2026-09-09 15:38:32 +0200

    feat(service): init QuestService file with functions

--- backend/src/main/java/laplateforme/guildboard/service/QuestService.java
+++ backend/src/main/java/laplateforme/guildboard/service/QuestService.java
@@ -1,5 +1,24 @@
 package laplateforme.guildboard.service;
 
+import java.util.List;
+import laplateforme.guildboard.dto.request.CreateQuestRequest;
+import laplateforme.guildboard.dto.request.UpdateQuestRequest;
+import laplateforme.guildboard.dto.response.QuestResponse;
+
 public interface QuestService {
 
+    // GET ALL QUESTS
+    List<QuestResponse> getAllQuests();
+
+    // GET QUEST BY ID
+    QuestResponse getQuestById(Long id);
+
+    // CREATE QUEST
+    QuestResponse createQuest(CreateQuestRequest request);
+
+    // UPDATE QUEST BY ID
+    QuestResponse updateQuestById(Long id, UpdateQuestRequest request);
+
+    // DELETE QUEST BY ID
+    void deleteQuestById(Long id);
 }
