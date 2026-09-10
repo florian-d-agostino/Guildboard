c3b9300c6cfd710329f22d6444a680e9f867be1c
Hykoo13 <haikmonossiandev@gmail.com>
2026-09-10 11:33:26 +0200

    feat(exception) : push exceptions files finish

--- backend/src/main/java/laplateforme/guildboard/exception/GlobablException.java
+++
@@ -1,7 +0,0 @@
-package laplateforme.guildboard.exception;
-
-public class GlobablException extends RuntimeException {
-    public GlobablException(String message) {
-        super(message);
-    }
-}
